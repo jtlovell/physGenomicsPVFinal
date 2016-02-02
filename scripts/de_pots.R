794ff3406326131b7454c7edef1f53cdcf6a1eb2
jtlovell <johntlovell@gmail.com>
2016-02-02 08:43:13 -0600

    Update data

--- scripts/de_pots.R
+++ scripts/de_pots.R
@@ -1,11 +1,25 @@
+library(devtools)
+install_github("jtlovell/physGenomicsPVFinal")
 library(physGenomicsPVFinal)
 rm(list=ls())
 data(potsGreenhouse)
 
+info<-infoPots
+counts<-countsPots
+id.recovery<-info$id[info$Treatment=="recovery"]
+sample.recovery<-info$Sample[info$id %in% id.recovery]
+info.recovery<-info[info$Sample %in% sample.recovery, ]
+counts.recovery<-counts[,sample.recovery]
+
+wd<-which(info$Treatment!="recovery")
+info.wetdry<-info[wd,]
+counts.wetdry<-counts[,wd]
+with(info.wetdry, table(Treatment, Time, Day))
+
 #################################
 # Part 4.1: Full model
 #################################
-stats<-pipeLIMMA(counts=countsPots, info=info, block=info$pot, formula="~ trt * time")
+stats<-pipeLIMMA(counts=counts.wetdry, info=info.wetdry, block=info$id, formula="~ Treatment * Time + Day")
 voom.trtByMonth<-stats$voom[["E"]]
 stats.fullmodel<-stats$simpleStats
 stats.allests<-stats$stats
